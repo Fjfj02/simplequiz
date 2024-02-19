bada8bb23fde5fce55aa2f7f430802b138c30d26
Fausto José Francisco Junior <faustojosefranciscojunior.fj@gmail.com>
2024-02-19 05:59:10 -0300

    comment add

--- question_model.py
+++ question_model.py
@@ -1,6 +1,7 @@
 class Question:
 
     def __init__(self, text, answer):
+        '''function to create class Question'''
         self.text = text
         self.answer = answer
         #register the question of the quiz
